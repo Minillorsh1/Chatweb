19d844485e756cf95fe5cafe37892387c7f49f70
Minillorsh1 <minillorsh@gmail.com>
2024-06-30 19:04:29 -0400

    no message

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,24 +1,24 @@
-//import { auth, provider } from "../firebase-config.js";
-//import { signInWithPopup } from "firebase/auth";
-//import "../styles/Auth.css";
-//import Cookies from "universal-cookie";
+import { auth, provider } from "../firebase-config.js";
+import { signInWithPopup } from "firebase/auth";
+import "../styles/Auth.css";
+import Cookies from "universal-cookie";
 
-//const cookies = new Cookies();
+const cookies = new Cookies();
 
-//export const Auth = ({ setIsAuth }) => {
-  //const signInWithGoogle = async () => {
-    //try {
-      //const result = await signInWithPopup(auth, provider);
-      //cookies.set("auth-token", result.user.refreshToken);
-      //setIsAuth(true);
-//    } catch (err) {
-//      console.error(err);
-//    }
-//  };
-//  return (
-//    <div className="auth">
-//      <p> Conectar con Google para Continuar </p>
-//      <button onClick={signInWithGoogle}> Conectar con Google </button>
-//    </div>
-//  );
-//};
+export const Auth = ({ setIsAuth }) => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className="auth">
+      <p> Conectar con Google para Continuar </p>
+      <button onClick={signInWithGoogle}> Conectar con Google </button>
+    </div>
+  );
+};
